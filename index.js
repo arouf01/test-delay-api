703dc30224fd5230a79c56f55de46069ada4074e
arouf01 <arouf10022@gmail.com>
2025-07-13 10:00:32 +0600

    ok

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const port = process.env.PORT || 8000;
+
+app.use(cors()); // <-- Call the cors() function
+
+app.get("/", (req, res) => {
+  setTimeout(() => {
+    res.json({ status: "ok" });
+  }, 60000); // 1-min delay
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
